8cee101a64d0aa64015278bedd91372a2dab0baf
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-10 09:00:45 -0300

    init project

---
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/MeuDinheirinhoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ohlavrac.meu_dinheirinho_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuDinheirinhoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuDinheirinhoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohlavrac/meu_dinheirinho_api/MeuDinheirinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohlavrac.meu_dinheirinho_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuDinheirinhoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
