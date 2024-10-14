208d9694b61b17cc98a9f7e3b1c5e88b5b4dab5f
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-14 10:32:35 -0300

    fix: users entity

--- src/main/java/com/ohlavrac/meu_dinheirinho_api/domain/entities/users/UsersEntity.java
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/domain/entities/users/UsersEntity.java
@@ -1,15 +1,15 @@
 package com.ohlavrac.meu_dinheirinho_api.domain.entities.users;
 
+import java.io.Serializable;
 import java.util.UUID;
-import java.util.List;
 
-import com.ohlavrac.meu_dinheirinho_api.domain.entities.transactions.TransactionEntity;
 import com.ohlavrac.meu_dinheirinho_api.domain.enums.UserRole;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,12 +17,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Table(name = "users")
-@Entity
+@Entity()
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsersEntity {
+public class UsersEntity implements Serializable {
     @Id
     @GeneratedValue
     private UUID id;
@@ -30,8 +30,7 @@ public class UsersEntity {
     private String name;
     private String email;
     private String password;
-    private UserRole role;
 
-    @OneToMany(mappedBy = "users")
-    private List<TransactionEntity> transactions;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
 }
