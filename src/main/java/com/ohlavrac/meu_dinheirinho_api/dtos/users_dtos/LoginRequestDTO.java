145ef4ef88706bfa1f1368cc284d8761c7708a4d
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-14 11:24:01 -0300

    feat: add login request and response

---
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/dtos/users_dtos/LoginRequestDTO.java
@@ -0,0 +1,8 @@
+package com.ohlavrac.meu_dinheirinho_api.dtos.users_dtos;
+
+public record LoginRequestDTO(
+    String email,
+    String password
+) {
+
+}
---
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/dtos/users_dtos/LoginResponseDTO.java
@@ -0,0 +1,8 @@
+package com.ohlavrac.meu_dinheirinho_api.dtos.users_dtos;
+
+public record LoginResponseDTO(
+    String email,
+    String token
+) {
+
+}
