0b03186960abd68f0ce7ddd6b670bb7ef5804edc
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-14 11:45:17 -0300

    feat: add auth controller with register endpoint

---
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/controllers/AuthController.java
@@ -0,0 +1,29 @@
+package com.ohlavrac.meu_dinheirinho_api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ohlavrac.meu_dinheirinho_api.dtos.users_dtos.RegisterRequestDTO;
+import com.ohlavrac.meu_dinheirinho_api.services.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService service;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> register(@RequestBody RegisterRequestDTO data) {
+        if (service.registerNewUser(data) == true) {
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+}
