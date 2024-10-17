b48b80f6343a1d14866c8ae926c76904941f9272
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-17 11:33:05 -0300

    feat: add user service

---
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/services/UserService.java
@@ -0,0 +1,34 @@
+package com.ohlavrac.meu_dinheirinho_api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.ohlavrac.meu_dinheirinho_api.domain.entities.users.UsersEntity;
+import com.ohlavrac.meu_dinheirinho_api.dtos.users_dtos.UserResponseDTO;
+import com.ohlavrac.meu_dinheirinho_api.infra.security.TokenService;
+import com.ohlavrac.meu_dinheirinho_api.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public UserResponseDTO getUserInfo(String token) {
+        UsersEntity user = getUSer(token);
+
+        UserResponseDTO responseDTO = new UserResponseDTO(user.getId(), user.getName(), user.getEmail(), user.getRole());
+
+        return responseDTO;
+    }
+
+    private UsersEntity getUSer(String token) {
+        String subject = tokenService.validateToken(token);
+        return repository.findByEmail(subject).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
