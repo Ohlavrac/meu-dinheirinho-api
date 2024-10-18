821c51c638f513b9bada777955d8de21a2ca4aa6
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-18 12:25:27 -0300

    feat: add route to get all user transaction

--- src/main/java/com/ohlavrac/meu_dinheirinho_api/controllers/TransactionController.java
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/controllers/TransactionController.java
@@ -1,7 +1,10 @@
 package com.ohlavrac.meu_dinheirinho_api.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ohlavrac.meu_dinheirinho_api.dtos.transaction_dtos.TransactionRequestDTO;
+import com.ohlavrac.meu_dinheirinho_api.dtos.transaction_dtos.TransactionResponseDTO;
 import com.ohlavrac.meu_dinheirinho_api.services.TransactionService;
 
 @RestController
@@ -23,5 +27,10 @@ public class TransactionController {
         this.transactionService.createTransaction(token, data);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/user/all")
+    public ResponseEntity<List<TransactionResponseDTO>> getUserTransactions(@RequestHeader("Authorization") String token) {
+        return ResponseEntity.ok(this.transactionService.getAllUserTransaction(token));
     }
 }
