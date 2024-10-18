5e4f6c0fa490612dcb5c3bc2189827add87bb70b
Ohlavrac <joaovicrcarvalho@gmail.com>
2024-10-18 12:26:16 -0300

    update

--- src/main/java/com/ohlavrac/meu_dinheirinho_api/repositories/TransactionRepository.java
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/repositories/TransactionRepository.java
@@ -1,13 +1,17 @@
 package com.ohlavrac.meu_dinheirinho_api.repositories;
 
 import java.util.UUID;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ohlavrac.meu_dinheirinho_api.domain.entities.transactions.TransactionEntity;
 
-@Repository
+@Repository  
 public interface TransactionRepository  extends JpaRepository<TransactionEntity, UUID>{
-
+    //@Query("SELECT t FROM TransactionEntity t WHERE t.users.id =: userID")
+    List<TransactionEntity> findByUsersId(UUID usersId);
 }
--- src/main/java/com/ohlavrac/meu_dinheirinho_api/repositories/UserRepository.java
+++ src/main/java/com/ohlavrac/meu_dinheirinho_api/repositories/UserRepository.java
@@ -4,6 +4,9 @@ import java.util.UUID;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ohlavrac.meu_dinheirinho_api.domain.entities.users.UsersEntity;
@@ -11,4 +14,8 @@ import com.ohlavrac.meu_dinheirinho_api.domain.entities.users.UsersEntity;
 @Repository
 public interface UserRepository extends JpaRepository<UsersEntity, UUID> {
     Optional<UsersEntity> findByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE UsersEntity u SET u.balance = :balance WHERE u.id = :id")
+    int updateBalance(@Param("id") UUID id, @Param("balance") double balance);
 }
